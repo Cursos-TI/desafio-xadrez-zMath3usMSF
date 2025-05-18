bb1af6131954a04a8f7cee1561d779b703d9bb24
Matheus Silva <48952393+zMath3usMSF@users.noreply.github.com>
2025-05-18 20:39:06 -0300

    Update xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -12,7 +12,7 @@ int main(){
     /*
         Bispo: Fazer a movimentação do Bispo de 5 casas para a direita e para cima.
     */
-    printf("Bispo:\n");
+    printf("\nBispo:\n");
     for(int i = 1; i <= 5; i++){
         printf("Direita\n");
         printf("Cima\n");
@@ -21,8 +21,21 @@ int main(){
     /*
         Rainha: Fazer a movimentação da Rainha de 8 casas para a esquerda.
     */
-    printf("Rainha:\n");
+    printf("\nRainha:\n");
     for(int i = 1; i <= 8; i++){
         printf("Esquerda\n");
+    }
+
+
+    /*
+        Cavalo: Fazer a movimentação do Cavalo de 2 casas para a baixo e 1 para esquerda.
+    */
+    printf("\nCavalo:\n");
+    for(int i = 1; i <= 2; i++){
+        printf("Baixo\n");
+        while(i == 2){
+            printf("Esquerda\n");
+            i++;
+        }
     }
 }
